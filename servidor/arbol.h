405b9e69bb1592425068e2078607e197874c2907
Gonzalo <gonzagatti@gmail.com>
2025-11-05 17:27:18 -0300

    Servidor: Limpieza primitivas de arbol

--- servidor/arbol.h
+++ servidor/arbol.h
@@ -23,45 +23,13 @@ typedef void(*tExtraer)(void *, void *);
 
 void crearArbol(tArbol *);
 int insertarArbolR(tArbol *, void *, unsigned, int(void*, void*));
-int insertarHojaI(tArbol *, void *, unsigned, int(void*, void*));
-tArbol *buscarElemento(tArbol *, void *, unsigned, int(void *, void *));
-
-void recorrerArbolPreOrdenR(tArbol *, void(void *));
-void recorrerArbolInOrdenR(tArbol *, void(void *, unsigned, void *), void *);
-void recorrerArbolPostOrdenR(tArbol *, void(void *));
-
-int contarHojas(tArbol *);
-int cantidadNodos(tArbol *);
-
-tArbol *buscarNodoClaveMayor(tArbol *);
-tArbol *buscarNodoClaveMenor(tArbol *);
-
-int cantNodosSubDerCondicion(tArbol *, void *, int(void *, void *));
-int contarNodosCondicion(tArbol *, void *, int(void *, void *));
-float promedioNodosClavePar(tArbol *, int(void *), void(int *, void *));
-int _promedioNodosClavePar(tArbol *, int *, int(void *), void(int *, void *));
-
-int saberAltura(tArbol *);
-
-void verHastaAltura(tArbol *, int, void(void *));
-void verNodosUnaAltura(tArbol *, int, void(void *));
-void verNodosDesdeAltura(tArbol *, int, void(void *));
-
-int eliminarNodo(tArbol *, void *, unsigned, int(void *, void *));
-int eliminarRaiz(tArbol *);
-
-int esArbolCompletoEnAltura(tArbol *, int);
-int esArbolCompleto(tArbol *);
-
 int cargarArbolDeBinDesord(tArbol *, char *, unsigned, int(void *, void *), tExtraer);
 int crearArchIndiceDeArbol(tArbol *, char *, tExtraer);
 void _insertarRegistroEnArchivoIndice(void *, unsigned, void *);
 int cargarArbolBalanceadoDeArchIndice(tArbol *, char *, unsigned);
 int _cargarArbolBalanceadoDeArchIndice(tArbol *, FILE *, unsigned, int, int);
 int crearArchBinIndiceDeArbol(tArbol *, char *);
-
 void vaciarArbol(tArbol *);
-
 int elementoEstaEnArbol(tArbol *arbol, void *, unsigned, int(void *, void *));
 
 #endif // ARBOL_H_INCLUDED
