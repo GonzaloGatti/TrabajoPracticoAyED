b114a46946e333f7b57e621a61b64dd6c847c623
tatimendez <159751352+tatimendez@users.noreply.github.com>
2025-11-05 20:21:47 -0300

    separo funciones

--- servidor/servidor_lib.h
+++ servidor/servidor_lib.h
@@ -23,22 +23,10 @@ SOCKET create_server_socket();
 // Ejecuta el bucle principal del servidor
 void run_server();
 
-int cmpIndicePorId(void *, void *);
-void extraerJugadorAIndice(void *, void *);
 int existeArchivo(const char *);
-int cmpUsuarios(void *, void *);
 int insercionArchivoDesYArbolBalanceado(tArbol *, tPartidaSrv *, char *, char *, int *);
-int obtenerProximoId(const char *, int *);
-int guardarPartidaArchivo(char *, tPartidaSrv *);
 
 int enviarRanking(SOCKET, tLista *);
 int send_all(SOCKET, const void *, int);
-
-int modificarCantPartidasJugador(char *, const tIndice, tPartidaSrv *);
-
-int crearRanking(tLista *, char *);
-int cmpId(void *, void *);
-int cmpPuntos(void *, void *);
-void acumularPuntos(void *, void *);
 
 #endif // SERVIDOR_LIB_H_INCLUDED
